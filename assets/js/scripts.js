7dcf84c0c5c60661b333c0d2a36a8c84b1da1441
hqthinh0 <hqthinh0@gmail.com>
2022-05-24 13:49:58 +0700

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -535,3 +535,12 @@ function sortPriceIncrease(n) {
    }
 }
 
+
+function hideLoader() {
+    $('#loadding').hide();
+}
+
+$(window).ready(hideLoader);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
